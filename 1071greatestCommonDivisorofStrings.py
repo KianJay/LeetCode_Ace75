8d40d06abf4b18a80d4454f7321afa97dda18e2d
duyoung jang <endud5118@gmail.com>
2024-12-11 10:11:29 +1300

    add1431

--- 1071greatestCommonDivisorofStrings.py
+++ 1071greatestCommonDivisorofStrings.py
@@ -1,3 +1,11 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
+        def is_divisible(s1, s2):
+            return s1 == s2 * (len(s1) // len(s2))
         
+        res = ""
+        for i in range(len(str2), 0, -1):
+            candidate = str2[:i]
+            if is_divisible(str1, candidate) and is_divisible(str2, candidate):
+                return candidate
+        return ""
